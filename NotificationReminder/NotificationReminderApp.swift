ad54fcbfd632fa230d12674f9e46a46ce2238203
Visalroth <chouvisalroth9999@gmail.com>
2025-09-12 16:12:27 +0700

    Initial Commit

---
+++ NotificationReminder/NotificationReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationReminderApp.swift
+//  NotificationReminder
+//
+//  Created by Visalroth on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct NotificationReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
